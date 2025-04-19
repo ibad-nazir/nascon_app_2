ea5a17824edeaab517b5b894f78094e61b2df9da
KelvinNoel <kelvin.noel1@gmail.com>
2025-04-19 12:29:51 +0500

    chnaged

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -1,4 +1,7 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 final darkbackground_color = Color(0xff1c1244);
 final secondary_color = Color(0xFF7c71e0);
+final background_color = Colors.white;

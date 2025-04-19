85fac9e4d72e82f3e6adc21db3515268c5b2b491
KelvinNoel <kelvin.noel1@gmail.com>
2025-04-19 12:53:20 +0500

    new change

--- lib/pages/Auth/signup/screens/signup.dart
+++ lib/pages/Auth/signup/screens/signup.dart
@@ -1,10 +1,42 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../../../common/constants.dart';
 
 class SignUp extends StatelessWidget {
-  const SignUp({super.key});
+  SignUp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Theme.of(context).brightness == Brightness.dark
+          ? darkbackground_color
+          : Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 80,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 12.0),
+            child:
+                IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.back)),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Text(
+              "Sign Up",
+              style: GoogleFonts.poppins(
+                  fontSize: 26, fontWeight: FontWeight.w800),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
